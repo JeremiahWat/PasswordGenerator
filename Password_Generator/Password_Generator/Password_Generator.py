fa61992a6d7d4015f4f3ba9458f599c31648a23c
JeremiahWat <36716640+JeremiahWat@users.noreply.github.com>
2026-06-27 16:51:11 -0500

    Add files via upload

--- Password_Generator/Password_Generator/Password_Generator.py
+++ Password_Generator/Password_Generator/Password_Generator.py
@@ -1,11 +1,19 @@
-import random
+import secrets
 import string
 
 def generate_password(length: int = 12):
+    if length < 12:
+        print("Password length should be at least 12 characters for better security.")
+        return False
     alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(alphabet) for i in range(length))
+    password = ''.join(secrets.choice(alphabet) for _ in range(length))
     return password
-
-password_length = int(input("Enter the desired password length (default is 12): "))
-password = (generate_password(password_length))
+password = False
+while password == False:
+    try:
+        password_length = int(input("Enter the desired password length (default is 12): "))
+    except:
+        print("Invalid input. Please enter a valid integer.")
+        continue
+    password = (generate_password(password_length))
 print(f"Generated password: {password}")
